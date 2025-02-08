cb47c77813e97ac7110f6eec9429b94125fb99ec
Ibodullayev_Hasan <ibodullayevh040@gmail.com>
2025-02-08 23:26:15 +0500

    fix

--- src/config/postgresFetchData.js
+++ src/config/postgresFetchData.js
@@ -2,21 +2,25 @@ let { Pool } = require(`pg`);
 require(`dotenv`).config();
 const url = process.env.URL;
 
-let pool = new Pool({
+const pool = new Pool({
   connectionString: url,
+  max: 10, // Maksimal bir vaqtda ochiq turadigan ulanishlar
+  idleTimeoutMillis: 30000, // 30 sekunddan so‘ng bo‘sh ulanishlar yopiladi
+  connectionTimeoutMillis: 2000, // 2 soniya ichida ulana olmasa, xato qaytaradi
 });
 
 // fetch_data logic
 const fetch_data = async (query, ...params) => {
-  let client = await pool.connect();
-
+  let client;
   try {
+    client = await pool.connect();
     let { rows } = await client.query(query, params);
     return rows;
   } catch (error) {
-    console.error("Fetch_Data: ", error);
+    console.error("Fetch_Data Error: ", error);
+    throw new Error("Database query failed"); // Xatoni tashlash
   } finally {
-    client.release();
+    if (client) client.release();
   }
 };
 
