cf076f75937907611a59446e6398ff7763d7e7de
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-04-27 22:12:00 +0500

    req type ga file qo'shildi

--- src/types/express.types.d.ts
+++ src/types/express.types.d.ts
@@ -1,9 +1,11 @@
 import { IUser } from "../interfaces";
+import { Multer } from "multer"
 
 declare global {
 	namespace Express {
 		export interface Request {
 			user?: IUser;
+			file?: Express.Multer.File;
 		}
 	}
 }
