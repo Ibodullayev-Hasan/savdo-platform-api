756abecd1987de2277a6cfc5eb89ba711e62db55
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-04-27 22:12:33 +0500

    cors optimision

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,15 @@ import cors from "cors";
 
 const expressApp = async (app: Application) => {
 
-	app.use(cors({
-		credentials:true,
-		// origin:["http://127.0.0.1:5500"],
-		origin:true,
-		// allowedHeaders:["Contn"]
-	}))
+	app.use(
+		cors({
+			credentials: true,
+			// origin:["http://127.0.0.1:5500"],
+			origin: true,
+			allowedHeaders: ["application/json"],
+			methods: ["PATCH", "GET", "POST", "OPTIONS", "DELETE"]
+		}))
+
 	connectDB()
 		.then(() => { console.log(`Mongodb | successfully connected!`) })
 		.catch((err) => { console.log(`Mongodb | ${err.message}`) })
