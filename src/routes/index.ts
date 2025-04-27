b3432733635e43e5cc7c5e76fd64c5514c05df8e
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-04-27 22:13:58 +0500

    file upload router chaqirildi

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express"
 import userRoute from "../modules/auth/route/auth.route"
+import fileUploadRoute from "../modules/file-upload/file-upload.route";
 const route = Router()
 
 route.use("/auth", userRoute);
+route.use("/avatar", fileUploadRoute);
 
 export default route;
