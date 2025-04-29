f66ba34a60459ae771592721208f141f7ab641b1
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-04-29 12:52:14 +0500

    deleted

--- src/modules/auth/route/auth.route.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express"
-import { AuthController } from "../controller/auth.controller"
-const userRoute = Router()
-
-userRoute.post("/sign-up", AuthController.signUp)
-
-export default userRoute;
