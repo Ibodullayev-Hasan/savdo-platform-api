9157a85c13a04b2a602987555fd88dcd1a5a4c94
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-04-29 12:55:41 +0500

    /user/profile add, /avatar/upload commit

--- src/middlewares/routes-error.middleware.ts
+++ src/middlewares/routes-error.middleware.ts
@@ -5,16 +5,14 @@ export const allowedRoutes: { [path: string]: string[] } = {
 	"/auth/sign-in": ["POST"],
 	"/auth/refresh": ["POST"],
 	"/auth/logout": ["POST"],
-	"/avatar/upload": ["POST"],
+	// "/avatar/upload": ["POST"],
 	"/user/profile": ["GET"],
 };
-
 
 export const routeErrorHandler = (req: Request, res: Response, next: NextFunction) => {
 	const methods = allowedRoutes[req.path];
 
 	if (methods) {
-		// route mavjud, lekin method ruxsat etilmagan
 		if (!methods.includes(req.method)) {
 			return res.status(405).json({
 				success: false,
@@ -22,7 +20,6 @@ export const routeErrorHandler = (req: Request, res: Response, next: NextFunctio
 			});
 		}
 	} else {
-		// route umuman yo‘q
 		return res.status(404).json({
 			success: false,
 			message: "Route not found"
