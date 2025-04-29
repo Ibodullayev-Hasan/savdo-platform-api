a496632c58d38a292e48c5226a58ea02a3359099
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-04-29 12:56:08 +0500

    user route add

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express"
-import userRoute from "../modules/auth/route/auth.route"
 import fileUploadRoute from "../modules/file-upload/file-upload.route";
+import authRoute from "../modules/auth/auth.route";
+import userRoute from "../modules/user/user.route";
 const route = Router()
 
-route.use("/auth", userRoute);
+route.use("/auth", authRoute);
+route.use("/user", userRoute);
 route.use("/avatar", fileUploadRoute);
 
 export default route;
